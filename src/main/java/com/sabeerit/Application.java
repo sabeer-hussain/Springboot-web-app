bf5a709c6baadea50e891519bfb9b66961eab448
sabeer-hussain <msabeerhussain.edu@gmail.com>
2021-02-07 23:14:36 +0530

    HIS-301:Related changes

--- src/main/java/com/sabeerit/Application.java
+++ src/main/java/com/sabeerit/Application.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	private Map<String, Object> cache = new HashMap<String, Object>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		int a = 10;
 	}
 
+	//HIS-301 changes
+	public void loadDataToCache() {
+		//logic
+	}
 }
